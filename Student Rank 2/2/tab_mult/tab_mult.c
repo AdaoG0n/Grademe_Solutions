c41b742657c42212100a7fb23eba7ea2ce161076
Adão Gonçalves <adao-goncalves@outlook.com>
2025-01-20 16:53:19 +0000

    Update tab_mult.c

--- Student Rank 2/2/tab_mult/tab_mult.c
+++ Student Rank 2/2/tab_mult/tab_mult.c
@@ -1,50 +1,52 @@
-#include <unistd.h>
+#include "unistd.h"
 
-int		ft_atoi(char *str)
+int     ft_atoi(char *str)
 {
-	int n = 0;
+        int     n = 0;
 
-	while (*str >= '0' && *str <= '9')
-	{
-		n = n * 10;
-		n = n + *str - '0';
-		++str;
-	}
-	return (n);
+        while (*str >= '0' && *str <= '9')
+        {
+                n = n * 10;
+                n = n + *str - '0';
+                str++;
+        }
+        return (n);
 }
 
-void	ft_putnbr(int n)
+void    ft_putnbr(int n)
 {
-	if (n >= 10)
-		ft_putnbr(n / 10);
+        char c;
 
-	char c = (n % 10) + '0';
-	write(1, &c, 1);
+        if (n >= 10)
+                ft_putnbr(n / 10);
+        c = (n % 10) + '0';
+        write(1, &c, 1);
 }
 
-void	tab_mult(char *str)
+void    tab_mult(char *str)
 {
-	int n;
-	int i = 1;
+        int     n;
+        int     i = 1;
 
-	n = ft_atoi(str);
-	while (i <= 9)
-	{
-		ft_putnbr(i);
-		write(1, " x ", 3);
-		ft_putnbr(n);
-		write(1, " = ", 3);
-		ft_putnbr(i * n);
-		write(1, "\n", 1);
-		++i;
-	}
+        n = ft_atoi(str);
+        while (i <= 9)
+        {
+                ft_putnbr(i);
+                write(1, " x ", 3);
+                ft_putnbr(n);
+                write(1, " = ", 3);
+                ft_putnbr( i * n);
+                write(1, "\n", 1);
+                i++;
+        }
 }
 
-int		main(int argc, char **argv)
+int     main(int argc, char **argv)
 {
-	if (argc == 2)
-		tab_mult(argv[1]);
-	else
-		write(1, "\n", 1);
-	return (0);
+        if (argc == 2)
+                tab_mult(argv[1]);
+        else
+                write(1, "\n", 1);
+        return (0);
 }
+
