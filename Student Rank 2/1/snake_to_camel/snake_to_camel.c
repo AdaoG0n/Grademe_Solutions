1de82e58d5f7d46a7d88ad6a28f7c680acbaf48d
Adão Gonçalves <adao-goncalves@outlook.com>
2025-01-21 10:11:32 +0000

    Update snake_to_camel.c

--- Student Rank 2/1/snake_to_camel/snake_to_camel.c
+++ Student Rank 2/1/snake_to_camel/snake_to_camel.c
@@ -1,20 +1,24 @@
-#include <stdlib.h>
-#include <unistd.h>
+#include "unistd.h"
 
-int main(int argc, char **argv)
+int     main(int argc, char **argv)
 {
-	if (argc == 2)
-	{
-		for (int i = 0; argv[1][i]; i += 1)
-		{
-			if (argv[1][i] == '_')
-			{
-				i += 1;
-				argv[1][i] -= 32;
-			}
-			write(STDOUT_FILENO, &argv[1][i], 1);
-		}
-	}
-	write(STDOUT_FILENO, "\n", 1);
-	return EXIT_SUCCESS;
+        int     i;
+        i = 0;
+
+        if(argc == 2)
+        {
+                while( argv[1][i] != '\0')
+                {
+                        if(argv[1][i] == '_')
+                        {
+                                i++;
+                                if(argv[1][i] >= 'a' && argv[1][i] <= 'z')
+                                        argv[1][i] -= 32;
+                        }
+                        write(1, &argv[1][i], 1);
+                        i++;
+                }
+        }
+        write(1, "\n", 1);
+        return (0);
 }
