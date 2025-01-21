2cc7c7ccf8178fe605756cbf2666743511d439bb
Adão Gonçalves <adao-goncalves@outlook.com>
2025-01-21 14:49:14 +0000

    Update first_word.c

--- Student Rank 2/0/first_word/first_word.c
+++ Student Rank 2/0/first_word/first_word.c
@@ -1,25 +1,19 @@
-#include <unistd.h>
+#include "unistd.h"
 
-void	ft_putchar(char c)
+int     main(int argc, char **argv)
 {
-	write(1, &c, 1);
-}
+        int     i = 0;
 
-int	main(int ac, char **av)
-{
-	int	i;
-
-	i = 0;
-	if (ac == 2)
-	{
-		while (av[1][i] == ' ' || av[1][i] == '\t')
-			i++;
-		while (av[1][i] != '\0' && av[1][i] != ' ' && av[1][i] != '\t')
-		{
-			ft_putchar(av[1][i]);
-			i++;
-		}
-	}
-	ft_putchar('\n');
-	return (0);
+        if(argc == 2)
+        {
+                while(argv[1][i] == ' ' || argv[1][i] == '\t')
+                        i++;
+                while (argv[1][i] != '\0' && argv[1][i] != ' ' && argv[1][i] != '\t')
+                {
+                        write(1, &argv[1][i], 1);
+                        i++;
+                }
+        }
+        write(1, "\n", 1);
+        return(0);
 }
