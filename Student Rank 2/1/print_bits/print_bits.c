20eed685847784edf78b85f4af2f9275fb3a9035
Adão Gonçalves <adao-goncalves@outlook.com>
2025-02-25 18:56:37 +0000

    Update print_bits.c

--- Student Rank 2/1/print_bits/print_bits.c
+++ Student Rank 2/1/print_bits/print_bits.c
@@ -1,14 +1,17 @@
-#include <unistd.h>
+#include "unistd.h"
 
 void    print_bits(unsigned char octet)
-{
-   int i;
-
-   for (i = 7; i >= 0; i--)
-   {
-        if (octet & (1 << i))
-            write (1, "1", 1);
-        else
-            write (1, "0", 1);
-   }
-}
+{       
+        int     i;
+        
+        i = 7;
+        
+        while ( i >= 0)
+        {       
+                if (octet & (1 << i))
+                        write(1, "1", 1);
+                else
+                        write(1, "0", 1);
+                i--;
+        }
+}       
