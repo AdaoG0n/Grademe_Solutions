05184c1374311875bd318cd822a79bcda4bf9d4e
Adão Gonçalves <adao-goncalves@outlook.com>
2025-03-12 09:48:33 +0000

    Update is_power_of_2.c

--- Student Rank 2/1/is_power_of_2/is_power_of_2.c
+++ Student Rank 2/1/is_power_of_2/is_power_of_2.c
@@ -15,7 +15,7 @@ int         is_power_of_2(unsigned int n)
 
 //////// Function with BITS ////////
 
-int is_power_of_2(unsigned int n) 
+int	is_power_of_2(unsigned int n) 
 {
     return (n > 0) && ((n & (n - 1)) == 0);
 }
